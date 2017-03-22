bbeeed2c3c42aec78675ed851a86cefd4d94c1ad
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-03-22 13:07:50 +0100

    Add: Random movie functionality

--- routes/movies.js
+++ routes/movies.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const request = require('request');
 const currencyFormatter = require('currency-formatter');
+const randomNumber = require('random-number-in-range');
 
 const URL = process.env.MOVIE_BASE_URL;
 const KEY = process.env.MOVIE_API_KEY;
 
 router.get('/lists/:page', function(req, res) {
   const PAGE = req.params.page;
+  console.log(PAGE)
   request(`${URL}/movie/${PAGE}?${KEY}`, function (error, response, body) {
     res.locals.data = clean.lists(JSON.parse(body));
     res.render('movies/list');
@@ -23,7 +25,11 @@ router.get('/detail/:id', function(req, res) {
 });
 
 router.get('/special/random', function(req, res) {
-  console.log('random');
+  const ID = randomNumber(1, 99999);
+  request(`${URL}/movie/${ID}?${KEY}`, function (error, response, body) {
+    res.locals.data = clean.detail(JSON.parse(body));
+    res.render('movies/detail');
+  });
 });
 
 const clean = {
