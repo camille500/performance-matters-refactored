19f5c1eff9a1f99196e664a221777c5962a1ea75
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-03-27 16:32:21 +0200

    Refactoring and added comments

--- public/sw.js
+++ public/sw.js
@@ -1,49 +1,54 @@
-const CORE_CACHE_NAME = 'moviespot-v1.0.0-core';
-const PAGE_CACHE_NAME = 'moviespot-v1.0.0-pages';
-const CORE_FILES = ['/assets/css/style.css', '/assets/css/movie_style.css'];
+/* CONFIGURATION
+-------------------------------------------------------------- */
+const config = {
+  CORE_CACHE_NAME: 'moviespot-v1.0.0-core',
+  PAGE_CACHE_NAME: 'moviespot-v1.0.0-pages',
+  CORE_FILES: ['/assets/css/style.css', '/assets/css/movie_style.css']
+}
 
 /* CACHE CORE SITE ASSETS
 -------------------------------------------------------------- */
 this.addEventListener('install', event => event.waitUntil(
-    caches.open(CORE_CACHE_NAME) // Open cache with name: moviespot-v1.0.0-core
-        .then(cache => cache.addAll(CORE_FILES)) // Add all core files to the cache
-        .then(this.skipWaiting())  // Create the new active worker
+  caches.open(config.CORE_CACHE_NAME) // Open cache with name: moviespot-v1.0.0-core
+  .then(cache => cache.addAll(config.CORE_FILES)) // Add all core files to the cache
+  .then(this.skipWaiting()) // Create the new active worker
 ));
 
 this.addEventListener('fetch', event => {
-    const request = event.request;
-    console.log(request);
-    if (request.mode === 'navigate') {
-        event.respondWith(
-            fetch(request)
-                .then(response => cachePage(request, response))
-                .catch(err => getCachedPage(request))
-                .catch(err => fetchCoreFile('/offline/'))
-        );
-    } else {
-        event.respondWith(
-            fetch(request)
-                .catch(err => fetchCoreFile(request.url))
-                .catch(err => fetchCoreFile('/offline/'))
-        );
-    }
+  const request = event.request; // All requests that are made
+  if (request.mode === 'navigate') { // These include the pages, not the core files
+    event.respondWith(
+      fetch(request)
+      .then(response => cachePage(request, response)) // Caches page that is not yet cached
+      .catch(err => getCachedPage(request)) // Get page from cache on error
+    );
+  } else { // Core files
+    event.respondWith(
+      fetch(request)
+      .catch(err => fetchCoreFile(request.url)) // Get core files from cache on error
+    );
+  }
 });
 
-function fetchCoreFile(url) {
-    return caches.open(CORE_CACHE_NAME)
-        .then(cache => cache.match(url))
-        .then(response => response ? response : Promise.reject());
+const fetchCoreFile = (URL) => {
+  return caches.open(config.CORE_CACHE_NAME)
+    .then(cache => cache.match(URL))
+    .then(response => response ? response : Promise.reject());
 }
 
-function getCachedPage(request) {
-    return caches.open(PAGE_CACHE_NAME)
-        .then(cache => cache.match(request))
-        .then(response => response ? response : Promise.reject());
+/* GET PAGE IF IT IS IN THE CACHE
+-------------------------------------------------------------- */
+const getCachedPage = (request) => {
+  return caches.open(config.PAGE_CACHE_NAME)
+    .then(cache => cache.match(request))
+    .then(response => response ? response : Promise.reject());
 }
 
-function cachePage(request, response) {
-    const clonedResponse = response.clone();
-    caches.open(PAGE_CACHE_NAME)
-        .then(cache => cache.put(request, clonedResponse));
-    return response;
+/* CACHE PAGE IF IT'S NOT CACHED YET
+-------------------------------------------------------------- */
+const cachePage = (request, response) => {
+  const clonedResponse = response.clone(); // Clone the response
+  caches.open(config.PAGE_CACHE_NAME) // Open the cache with all pages, not the core-files
+    .then(cache => cache.put(request, clonedResponse)); // Add clone of file to cache
+  return response;
 }
